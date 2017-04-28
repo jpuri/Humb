2273edc64aa634a25d0116e2eceb251c67ec7512
jpuri <jyotipuri@gmail.com>
2017-04-29 00:12:57 +0530

    copy content to new node

--- packages/core/src/helpers/EditorState.js
+++ packages/core/src/helpers/EditorState.js
@@ -34,15 +34,14 @@ function addNode(editorState) {
   const { node } = getActiveBlockNode(editorState);
   let nodes = editorState.get('nodes');
   const key = keyGen();
-  let newNode = {
+  let newNode = fromJS({
     type: node.get('type'),
     key: key,
     depth: 0,
     children: {},
-  };
-  nodes = nodes.set(key, fromJS(newNode));
-  // todo: find node size here
-  if (true) {
+  });
+  nodes = nodes.set(key, newNode);
+  if (cursor === getNodeSize(node)) {
     const childKey = keyGen();
     const childNode = {
       type: 'text',
@@ -51,18 +50,49 @@ function addNode(editorState) {
       parent: key,
       content: undefined,
     };
-    nodes = nodes.set(key, nodes.get(key).set('children', fromJS({[childKey]: { key: childKey, start: 0, end: 0 }})));
+    nodes = nodes.set(key, newNode.set('children', fromJS({[childKey]: { key: childKey, start: 0, end: 0 }})));
     nodes = nodes.set(childKey, fromJS(childNode));
+  } else {
+    const children = node.get('children').toList();
+    for(let i = 0;i < children.size;i++) {
+      const c = children.get(i);
+      if (c.get('start') <= cursor && c.get('end') >= cursor) {
+        let cNode = nodes.get(c.get('key'));
+        const content = cNode.get('content');
+        cNode = cNode.set('content', `${content.substr(0, cursor)}`);
+        nodes = nodes.set(c.get('key'), cNode);
+        const childKey = keyGen();
+        const newContent = content.substr(cursor);
+        const childNode = fromJS({
+          type: cNode.get('type'),
+          key: childKey,
+          depth: 1,
+          parent: key,
+          content: newContent,
+        });
+        nodes = nodes.set(key, newNode.set('children', fromJS({[childKey]: { key: childKey, start: 0, end: newContent.length }})));
+        nodes = nodes.set(childKey, childNode);
+        break;
+      }
+    };
   }
   return editorState.set('nodes', nodes);
 };
 // todo123: If old node is broken in-between transfer content to new node.
+// remove selection at time of enter
+
+function getNodeSize(node) {
+  const lastChild = node.get('children').maxBy(child => child.get('end'));
+  return lastChild.get('end');
+}
 
 function updateContent(editorState, key) {
   const cursor = window.getSelection().focusOffset;
   let { key: nodeKey, node } = getActiveNode(editorState);
   let nodes = editorState.get('nodes');
-  node.get('children').toList().forEach(c => {
+  const children = node.get('children').toList();
+  for(let i = 0;i < children.size;i++) {
+    const c = children.get(i);
     if (c.get('start') <= cursor && c.get('end') >= cursor) {
       let cNode = nodes.get(c.get('key'));
       const content = cNode.get('content');
@@ -70,8 +100,9 @@ function updateContent(editorState, key) {
       node = node.set('children', node.get('children').set(c.get('key'), c.set('end', c.get('end') + 1)));
       nodes = nodes.set(c.get('key'), cNode);
       nodes = nodes.set(nodeKey, node);
+      break;
     }
-  });
+  };
   return editorState.set('nodes', nodes);
 };
 // if node is not block type update node after this node also, use getActiveBlockNode and write recursive function to insert content
